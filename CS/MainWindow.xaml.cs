e9446d856c4bef772d1a8ccea24c1baf951c4558
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using DevExpress.XtraScheduler;
+
+namespace SchedulerThemeBasedStylesWpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            GenerateResources();
+        }
+
+        private void GenerateResources() {
+            schedulerControl1.Storage.BeginUpdate();
+            Resource res = schedulerControl1.Storage.ResourceStorage.CreateResource(0);
+            res.Caption = "First Resource";
+            schedulerControl1.Storage.ResourceStorage.Add(res);
+
+            res = schedulerControl1.Storage.ResourceStorage.CreateResource(1);
+            res.Caption = "Second Resource";
+            schedulerControl1.Storage.ResourceStorage.Add(res);
+            schedulerControl1.Storage.EndUpdate();
+        }
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Windows
+Imports DevExpress.XtraScheduler
+
+Namespace SchedulerThemeBasedStylesWpf
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+
+            GenerateResources()
+        End Sub
+
+        Private Sub GenerateResources()
+            schedulerControl1.Storage.BeginUpdate()
+            Dim res As Resource = schedulerControl1.Storage.ResourceStorage.CreateResource(0)
+            res.Caption = "First Resource"
+            schedulerControl1.Storage.ResourceStorage.Add(res)
+
+            res = schedulerControl1.Storage.ResourceStorage.CreateResource(1)
+            res.Caption = "Second Resource"
+            schedulerControl1.Storage.ResourceStorage.Add(res)
+            schedulerControl1.Storage.EndUpdate()
+        End Sub
+    End Class
+End Namespace
